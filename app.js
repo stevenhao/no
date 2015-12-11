4128e3cfd0fe7bc7261c8af1e0f670fc85ca2f7c
stevenhao <stevenhao@gmail.com>
2015-12-10 20:34:58 -0500

    initial commit
    - add .gitignore, package.json
    - add bower, bower.json
    - prototype app.js, views/index.html

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var http = require('http');
+
+var app = express();
+var port = process.env.PORT || 4000;
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/bower_components'));
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// var server = require('./server');
+// server.attach(app);
+
+var httpServer = http.createServer(app);
+httpServer.listen(port);
