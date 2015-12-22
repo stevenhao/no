1443f7c7a8977630c7722050e2946f9ed8edbe9d
stevenhao <stevenhao@gmail.com>
2015-12-22 09:33:00 -0800

    small edits for deploy

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ var http = require('http');
 
 var app = express();
 var port = process.env.PORT || 4000;
+var print = console.log.bind(console);
+print('trying to start server.')
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/html/index.html');
+});
+
+app.on('uncaughtException', function (err) {
+  console.error(err);
+  console.log("Node NOT Exiting...");
 });
 
 // var server = require('./server');
@@ -14,3 +21,4 @@ app.get('/', function(req, res) {
 
 var httpServer = http.createServer(app);
 httpServer.listen(port);
+print('server started on port', port);
